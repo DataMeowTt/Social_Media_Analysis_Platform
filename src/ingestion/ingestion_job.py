672907dc8da48c050e004dbb4274f335f261985c
TuanTuVinhPhuc <trantuann1809@gmail.com>
2026-04-09 23:52:05 +0700

    refactor: data ingestion

--- src/ingestion/ingestion_job.py
+++ src/ingestion/ingestion_job.py
@@ -1,17 +1,22 @@
+import asyncio
+import aiohttp
+import json
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timezone
+
 from src.ingestion.api.fetcher import TwitterDataFetcher
 from src.ingestion.api.client import TwitterAPIClient   
 from src.storage.s3.uploader import upload_to_s3
 from src.ingestion.api.enums.query_type import QueryType
-
-from dotenv import load_dotenv
 from src.utils.config_loader import load_config
 from src.utils.logger import get_logger 
-from datetime import datetime, timezone
-import os
 
 load_dotenv()
 CONFIG = load_config()
 logger = get_logger(__name__)
+
+BUFFER_LIMIT_BYTES = 150 * 1024 * 1024 # 150MB
 
 def add_ingestion_date(records: list[dict], ingestion_time: datetime) -> list[dict]:
     ingestion_date = ingestion_time.strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -20,41 +25,101 @@ def add_ingestion_date(records: list[dict], ingestion_time: datetime) -> list[di
 
     return records
 
-def run_ingestion_tweets(query : str, query_type: QueryType, max_pages: int = 20) -> tuple[str, list[dict], datetime]:
-    logger.info(f"Starting ingestion for query: {query}, query_type: {query_type}, max_pages: {max_pages}") 
-   
+class IngestionBuffer:
+    def __init__(self):
+        self._data: list[dict] = []
+        self._size_bytes: int = 0
+        self._lock = asyncio.Lock()
+
+    async def add(self, records: list[dict]) -> int:
+        chunk_size = sum(len(json.dumps(r, ensure_ascii=False).encode("utf-8")) for r in records)
+        
+        async with self._lock:
+            self._data.extend(records)
+            self._size_bytes += chunk_size
+            return self._size_bytes
+
+    async def flush(self) -> tuple[list[dict], int]:
+        async with self._lock:
+            data, size = self._data.copy(), self._size_bytes
+            self._data.clear()
+            self._size_bytes = 0
+            
+            return data, size
+        
+async def _upload_chunk(chunk: list[dict], ingestion_time: datetime, dataset: str, layer: str) -> str:
+    loop = asyncio.get_event_loop()
+
+    # Offload the blocking upload to a thread pool executor
+    s3_path = await loop.run_in_executor(
+        None,
+        lambda: upload_to_s3(
+            data=chunk,
+            dataset=dataset,
+            layer=layer,
+            ingestion_time=ingestion_time
+        )
+    )
+    logger.info(f"Chunk of {len(chunk)} records uploaded to S3 at {s3_path}")
+    return s3_path
+
+async def run_ingestion_tweets(query : str, query_type: QueryType, tweets_number: int) -> list[str]:
+    ingestion_time = datetime.now(timezone.utc)
+    buffer = IngestionBuffer()
+    upload_tasks: list[asyncio.Task] = []
+
     client = TwitterAPIClient(
-        api_key = os.getenv("MY_API_KEY"),
-        base_url = CONFIG["api"]["url"]
+        api_key=os.getenv("MY_API_KEY"),
+        base_url=CONFIG["api"]["url"]
     )
     fetcher = TwitterDataFetcher(client)
-
-    ingestion_time = datetime.now(timezone.utc)
-
-    logger.info("Fetching tweets from Twitter API...")
-    tweets = fetcher.fetch_tweets(
-        query=query, 
-        query_type=query_type, 
-        max_pages=max_pages
-    )
-
-    if not tweets:
-        logger.warning("No tweets fetched for the given query and query type.")
-        return None
     
-    logger.info(f"Fetched {len(tweets)} tweets. Adding ingestion date...")
-    
-    tweets = add_ingestion_date(tweets, ingestion_time)
-    
-    logger.info("Uploading data to S3...")
-    s3_path = upload_to_s3(
-        data=tweets,
-        dataset="tweets",
-        layer="raw",
-        ingestion_time=ingestion_time
-    )
-    logger.info(f"Ingestion completed. Data uploaded to: {s3_path}")
-    
-    logger.info(f"Total ingestion time: {(datetime.now(timezone.utc) - ingestion_time).total_seconds()} seconds")
-    return s3_path, tweets, ingestion_time
+    logger.info(f"Starting ingestion for query: '{query}' with type: {query_type.name} aiming for {tweets_number} tweets")
 
+    async with aiohttp.ClientSession() as session:
+        async for batch in fetcher.fetch_tweets(session, query, query_type, tweets_number):
+            batch = add_ingestion_date(batch, ingestion_time)
+            current_size = await buffer.add(batch)
+
+            if current_size >= BUFFER_LIMIT_BYTES:
+                chunk, size = await buffer.flush()
+                logger.info(f"Buffer limit reached ({size} bytes). Uploading chunk of {len(chunk)} records to S3...")
+
+                upload_tasks.append(
+                    asyncio.create_task(
+                        _upload_chunk(
+                            chunk=chunk,
+                            ingestion_time=ingestion_time,
+                            dataset="tweets",
+                            layer="raw"
+                        )
+                    )
+                )
+
+    # Final flush after fetching is done
+    remaining, size = await buffer.flush()
+    if remaining:
+        logger.info(f"final flushing: {size / 1e6:.1f} MB ({len(remaining)} tweets)")
+        upload_tasks.append(
+            asyncio.create_task(
+                _upload_chunk(
+                    chunk=remaining,
+                    ingestion_time=ingestion_time,
+                    dataset="tweets",
+                    layer="raw"
+                )
+            )
+        )
+
+    results = await asyncio.gather(*upload_tasks, return_exceptions=True)
+    s3_paths = []
+    for r in results:
+        if isinstance(r, Exception):
+            logger.error(f"Error during upload: {r}")
+        else:
+            s3_paths.append(r)
+
+    elapsed = (datetime.now(timezone.utc) - ingestion_time).total_seconds()
+    logger.info(f"Ingestion completed in {elapsed:.2f} seconds with {len(s3_paths)} chunks uploaded to S3")
+
+    return s3_paths
--- src/storage/s3/uploader.py
+++ src/storage/s3/uploader.py
@@ -1,41 +1,67 @@
 import boto3
 import json
+import gzip
+import io
+import pyarrow as pa
+import pyarrow.parquet as pq
 from datetime import datetime, timezone
 from src.storage.s3.partitioning import get_s3_key
 from src.utils.config_loader import load_config, load_table_config
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 def upload_to_s3(data: list[dict], dataset: str, layer: str, ingestion_time: datetime = None) -> str:
     config = load_config()
     table_config = load_table_config(layer)
 
     bucket = config["s3"]["bucket_name"]
-    region = config["aws"]["region"]
     fmt = table_config["format"]
     partition_cols = table_config["partition_cols"]
 
     if ingestion_time is None:
         ingestion_time = datetime.now(timezone.utc)
 
+    if fmt == "json":
+        filename = f"{dataset}_{ingestion_time.strftime('%Y%m%d_%H%M%S')}.json.gz"
+    elif fmt == "parquet":
+        filename = f"{dataset}_{ingestion_time.strftime('%Y%m%d_%H%M%S')}.parquet"
+    else:
+        logger.error(f"Unsupported format: {fmt}")
+        raise ValueError(f"Unsupported format: {fmt}")
+
     s3_key = get_s3_key(
         layer=layer,
         dataset=dataset,
-        filename=f"{dataset}_{ingestion_time.strftime('%Y%m%d_%H%M%S')}.{fmt}",
+        filename=filename,
         ingestion_time=ingestion_time,
         partition_cols=partition_cols
     )
 
-    s3 = boto3.client("s3", region_name=region)
+    s3 = boto3.client("s3", region_name=config["aws"]["region"])
     
     if fmt == "json":
+        body = gzip.compress(json.dumps(data, ensure_ascii=False).encode("utf-8"))
         s3.put_object(
-           Bucket=bucket,
-           Key=s3_key,
-           Body=json.dumps(data, ensure_ascii=False).encode("utf-8"),
-           ContentType="application/json"
+            Bucket=bucket,
+            Key=s3_key,
+            Body=body,
+            ContentType="application/json",
+            ContentEncoding="gzip"
         )
     elif fmt == "parquet":
-        # TODO Implement parquet upload later.
-        pass
+        table = pa.Table.from_pylist(data)
+        buffer = io.BytesIO()
+        pq.write_table(table, buffer, compression="snappy")
+        buffer.seek(0)
+        s3.put_object(
+            Bucket=bucket,
+            Key=s3_key,
+            Body=buffer.read(),
+            ContentType="application/octet-stream"
+        )
+
+    logger.info(f"Data uploaded to S3 at {s3_key}") 
 
     return f"s3://{bucket}/{s3_key}"
 
