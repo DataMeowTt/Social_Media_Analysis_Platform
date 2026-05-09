51c52007eca91318ad0bf99186e2ac14ee7d8d0b
Tran Anh Tuan <169423503+DataMeowTt@users.noreply.github.com>
2026-05-09 15:30:00 +0700

    refactor: change to regexp for pasing timestamp

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,8 +1,13 @@
 from pyspark.sql import Column
 from pyspark.sql import functions as F
 
-# TODO: change format when host API changes
-TWITTER_DATE_FORMAT = "EEE MMM dd HH:mm:ss Z yyyy"
 
 def parse_twitter_timestamp(col: Column) -> Column:
-    return F.to_timestamp(col, TWITTER_DATE_FORMAT)
+    # "Fri May 09 08:14:27 +0000 2026" → "2026-May-09 08:14:27"
+    # EEE and trailing-year format is not supported by Spark 3.0+ DateTimeFormatter
+    reformatted = F.regexp_replace(
+        col,
+        r'^\w+\s+(\w+)\s+(\d+)\s+(\d+:\d+:\d+)\s+\S+\s+(\d+)$',
+        '$4-$1-$2 $3',
+    )
+    return F.to_timestamp(reformatted, "yyyy-MMM-d HH:mm:ss")
